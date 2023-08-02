73a264ff78353b40dbdfe67f966f4474d0e494d7
Mike Zimin <mikeziminio@gmail.com>
2023-08-02 16:58:54 +0400

    comments block to pep8

--- src/asgi.py
+++ src/asgi.py
@@ -1,4 +1,4 @@
-from typing import TypedDict, Iterable
+from typing import TypedDict, Iterable, Any
 from enum import StrEnum
 from pydantic import BaseModel
 
@@ -11,10 +11,13 @@ class ASGIType(StrEnum):
 
 
 class ASGIVersion(TypedDict):
-    version: str                    # Version of the ASGI spec
-    spec_version: str = "2.0"       # Version of the ASGI HTTP spec this server understands;
-                                    # one of "2.0", "2.1", "2.2" or "2.3".
-                                    # Optional; if missing assume 2.0.
+    # Version of the ASGI spec
+    version: str
+
+    # Version of the ASGI HTTP spec this server understands;
+    # one of "2.0", "2.1", "2.2" or "2.3".
+    # Optional; if missing assume 2.0.
+    spec_version: str = "2.0"
 
 
 class HTTPVersion(StrEnum):
@@ -24,30 +27,54 @@ class HTTPVersion(StrEnum):
 
 
 class ASGIScope(BaseModel):
-    type: ASGIType                  # "http" или "websocket"
+
+    # "http" or "websocket"
+    type: ASGIType
+
     asgi: ASGIVersion
-    http_version: HTTPVersion       # For websocket: NOT "1.0", ONLY "1.1" or "2"
-    method: str                     # The HTTP method name, uppercased
 
-    scheme: str                     # URL scheme portion (LIKELY "http" / "https" / "ws" / "wss").
-                                    # Optional (but must not be empty).
-                                    # For "http" type: default is "http".
-                                    # For "websocket" type: default id "ws".
+    # For websocket: NOT "1.0", ONLY "1.1" or "2"
+    http_version: HTTPVersion
 
-    path: str                       # HTTP request target excluding any query string,
-                                    # with percent-encoded sequences and UTF-8 byte sequences decoded into characters.
+    # The HTTP method name, uppercased
+    method: str
 
-    raw_path: str | None = None     # The original HTTP path component unmodified from the bytes
-                                    # that were received by the web server.
-                                    # Some web server implementations may be unable to provide this.
-                                    # Optional; if missing defaults to None.
+    # URL scheme portion (LIKELY "http" / "https" / "ws" / "wss").
+    # Optional (but must not be empty).
+    # For "http" type: default is "http".
+    # For "websocket" type: default id "ws".
+    scheme: str
 
-    query_string: str = ""          # URL portion after the ?.
-                                    # Optional; if missing or None default is empty string.
+    # HTTP request target excluding any query string,
+    # with percent-encoded sequences and UTF-8 byte sequences decoded into characters.
+    path: str
 
-    root_path: str = ""             # The root path this application is mounted at;
-                                    # same as SCRIPT_NAME in WSGI.
-                                    # Optional; if missing defaults to "".
+    # The original HTTP path component unmodified from the bytes
+    # that were received by the web server.
+    # Some web server implementations may be unable to provide this.
+    # Optional; if missing defaults to None.
+    raw_path: str | None = None
 
-    headers: Iterable[bytes, bytes]     # headers
+    # URL portion after the ?.
+    # Optional; if missing or None default is empty string.
+    query_string: str = ""
+
+    # The root path this application is mounted at;
+    # same as SCRIPT_NAME in WSGI.
+    # Optional; if missing defaults to "".
+    root_path: str = ""
+
+    # headers
+    headers: Iterable[bytes, bytes]
+
+    # client [host, port]
+    client: Iterable[str, int]
+
+    # server [host, port] or server [unix socket, None]
+    server: Iterable[str, int | None]
+
+    # A copy of the namespace passed into
+    # the lifespan corresponding to this request. (See Lifespan Protocol).
+    # Optional; if missing the server does not support this feature.
+    state: dict[str, Any] | None
 
